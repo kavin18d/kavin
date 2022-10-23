92219df73ab05ad37946d7289ab16df5fbcf01fb
kavin18d <kavin@datadna.in>
2022-10-23 23:20:24 +0530

    first project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+  const getNews =() =>{
+    Axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=da5e11e4f5c14c1aac34f67d00097d8c&q=india")
+    .then((response)=>{
+      console.log(response)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <button className='App' onClick={getNews}>GET NEWS</button>
     </div>
+    </>
   );
 }
 
